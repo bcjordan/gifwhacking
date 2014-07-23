cef4efdf6c30de3c9584a05bab06834827c6f4b9
Brian Jordan <bcjordan@gmail.com>
2014-07-22 23:47:46 -0700

    Add less placeholder-y-copy - Fix grunt hint warning

--- server/config/seed.js
+++ server/config/seed.js
@@ -26,10 +26,10 @@ Game.find({}).remove(function() {
     Game.findOne({name: 'Game 1'},
       function (err, game) {
         game.messages.push(
-            { text: 'a game message' }
-            , { text: 'another game message' }
-            , { text: 'a great game message' }
-            , { text: 'a good game message' }
+            { text: 'a game message' },
+            { text: 'another game message' },
+            { text: 'a great game message' },
+            { text: 'a good game message' }
         );
         game.save();
         console.log(game);
@@ -40,23 +40,17 @@ Game.find({}).remove(function() {
 
 Thing.find({}).remove(function() {
   Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
+    name : 'Discover Sweet Gifs',
+    info : 'Just think of your collection after playing'
   }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
+    name : 'Prove your intelligence',
+    info : 'You always thought you were great at Googling stuff'
   }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
+    name : 'LOL WUT',
+    info : "You'll say that sometimes"
+  }, {
+    name : 'Huh, interesting...',
+    info : 'Discover corners of the internet, scary and wonderful'
   });
 });
 
